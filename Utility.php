9a6fea2537c74424a8a8682b613906d805b51c80
OmegaExtern <omegaextern@live.com>
2015-04-19 15:22:21 +0200

    Utility reordered.

--- Utility.php
+++ Utility.php
@@ -15,6 +15,17 @@ class Utility
     }*/
 
     /**
+     * @param $value double|float|number Value to clamp.
+     * @param $max double|float|number Maximum range.
+     * @param $min double|float|number Minimum range.
+     * @return mixed double|float|number Returns clamped value (min <= value <= max).
+     */
+    public static function clamp($value, $max, $min)
+    {
+        return min(max($value, $min), $max);
+    }
+
+    /**
      * @param $value bool Boolean to convert.
      * @return int Returns 1 if the given value is TRUE; otherwise 0.
      */
@@ -24,12 +35,39 @@ class Utility
     }
 
     /**
+     * @param $value int Integer to convert.
+     * @return bool Returns TRUE if the given value is greater than 0; otherwise FALSE.
+     */
+    public static function int2bool($value)
+    {
+        return self::clamp($value, 0, 1) === 1;
+    }
+
+    /**
+     * @param $value string String to sanitize.
+     * @return string Returns sanitized string of the given value.
+     */
+    public static function sanitizeInput($value)
+    {
+        return trim(str_replace('\n', '', filter_var(htmlspecialchars(stripslashes(strip_tags(trim($value))), ENT_QUOTES, 'UTF-8'), FILTER_SANITIZE_STRING)));
+    }
+
+    /**
      * @param $community_identifier string Value to validate.
      * @return bool Returns TRUE if the given community_identifier is valid; otherwise FALSE.
      */
     public static function isValidCommunityId($community_identifier)
     {
         return preg_match('/^(7656119[0-9]{10})$/A', strval($community_identifier)) === 1;
+    }
+
+    /**
+     * @param $name string Name of the player.
+     * @return bool Returns TRUE if given name is valid; otherwise FALSE.
+     */
+    public static function isValidName($name)
+    {
+        return preg_match('/' . NAME_PATTERN . '/A', strval($name)) === 1;
     }
 
     /**
@@ -57,35 +95,6 @@ class Utility
     }
 
     /**
-     * @param $value int Integer to convert.
-     * @return bool Returns TRUE if the given value is greater than 0; otherwise FALSE.
-     */
-    public static function int2bool($value)
-    {
-        return self::clamp($value, 0, 1) === 1;
-    }
-
-    /**
-     * @param $value double|float|number Value to clamp.
-     * @param $max double|float|number Maximum range.
-     * @param $min double|float|number Minimum range.
-     * @return mixed double|float|number Returns clamped value (min <= value <= max).
-     */
-    public static function clamp($value, $max, $min)
-    {
-        return min(max($value, $min), $max);
-    }
-
-    /**
-     * @param $value string String to sanitize.
-     * @return string Returns sanitized string of the given value.
-     */
-    public static function sanitizeInput($value)
-    {
-        return trim(str_replace('\n', '', filter_var(htmlspecialchars(stripslashes(strip_tags(trim($value))), ENT_QUOTES, 'UTF-8'), FILTER_SANITIZE_STRING)));
-    }
-
-    /**
      * @param $steam_identifier string Steam ID to convert into community ID.
      * @return string Returns community ID representation of the given Steam ID.
      */
@@ -95,14 +104,5 @@ class Utility
             throw new InvalidArgumentException('steam_identifier is not valid.');
         }
         return bcadd(bcadd(bcmul(substr($steam_identifier, 10), '2'), self::Long), substr($steam_identifier, 8, 1));
-    }
-
-    /**
-     * @param $name string Name of the player.
-     * @return bool Returns TRUE if given name is valid; otherwise FALSE.
-     */
-    public static function isValidName($name)
-    {
-        return preg_match('/' . NAME_PATTERN . '/A', strval($name)) === 1;
     }
 }
