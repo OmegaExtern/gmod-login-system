dafe4f9f1adecbc5b1a385948e22dd2913b1cec4
OmegaExtern <omegaextern@live.com>
2015-04-19 07:40:50 +0200

    First update.

---
+++ Constants.php
@@ -0,0 +1,10 @@
+<?php
+const DATABASE_DNS = 'mysql:host=127.0.0.1;port=3306;dbname=system;charset=utf8;';
+const DATABASE_OPTIONS = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_EMULATE_PREPARES => false, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'];
+const DATABASE_PASSWD = '';
+const DATABASE_USERNAME = 'root';
+const MAX_COMMUNITY_ID_LENGTH = 17;
+const MAX_NAME_LENGTH = 24;
+const MAX_STEAM_ID_LENGTH = 19;
+const MIN_NAME_LENGTH = 4;
+const NAME_PATTERN = '^([A-Za-z][0-9A-Za-z]{' . (MIN_NAME_LENGTH - 1) . ',' . (MAX_NAME_LENGTH - 1) . '})$';
