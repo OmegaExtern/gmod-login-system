a7ac24700ada8404c187a323163c41ec9c7b4390
OmegaExtern <omegaextern@live.com>
2015-04-24 20:30:47 +0200

    Fixed cvarsx module. VGUI now supports all possible screen resolutions (also auto-adapts when screen resolution is changed).

--- lua/convars.lua
+++ lua/convars.lua
@@ -21,10 +21,37 @@ login_sys =
     hook_prefix = "login_system_"
 }
 require("cvarsx") -- require cvarsx module.
+login_sys.vgui =
+{
+    width = 640.0,
+    height = 480.0
+}
 login_sys.convars = {}
---                                 CreateTwoStateClientConVar(convar name, default, shouldsave, userdata, funcOff, funcOn
+login_sys.convars.vgui = cvarsx.CreateTwoStateClientConVar(login_sys.convar_prefix .. "vgui", 0, false, false, nil, function(convar_name) -- value_old, value_new
+if not IsValid(GetHUDPanel()) then
+    return
+end
+login_sys.vgui.width_multiplier = math.Round(ScrW() / login_sys.vgui.width)
+login_sys.vgui.height_multiplier = math.Round(ScrH() / login_sys.vgui.height)
+login_sys.vgui.dframe = vgui.Create("DFrame", GetHUDPanel())
+login_sys.vgui.dframe.btnMaxim:SetVisible(false) -- Hide maximize button.
+login_sys.vgui.dframe.btnMinim:SetVisible(false) -- Hide minimize button.
+login_sys.vgui.dframe:SetBackgroundBlur(true)
+--login_sys.vgui.dframe:SetDraggable(false)
+login_sys.vgui.dframe:SetScreenLock(true)
+login_sys.vgui.dframe:SetSize(616.0 * login_sys.vgui.width_multiplier, 456.0 * login_sys.vgui.height_multiplier)
+login_sys.vgui.dframe:SetTitle("Login System VGUI")
+login_sys.vgui.dframe:SetZPos(32767)
+login_sys.vgui.dframe:Center()
+login_sys.vgui.dframe:MakePopup()
+login_sys.vgui.dframe.OnClose = function()
+    RunConsoleCommand(convar_name, 0) -- Reset vgui convar.
+end
+end, true, true, false)
 login_sys.convars.enabled = cvarsx.CreateTwoStateClientConVar(login_sys.convar_prefix .. "enabled", 1, false, false, function() -- convar_name, value_old, value_new
 hook.Remove("OnPlayerChat", login_sys.hook_prefix .. "OnPlayerChat")
+hook.Remove("HUDPaint", login_sys.hook_prefix .. "OnScreenResolutionChange")
+hook.Remove("OnScreenResolutionChanged", login_sys.hook_prefix .. "OnScreenResolutionChanged")
 DebugInfo(1, "Login System: Disabled.")
 end, function() -- convar_name, value_old, value_new
 --[[
@@ -42,13 +69,13 @@ RETURNS:
 hook.Add("OnPlayerChat", login_sys.hook_prefix .. "OnPlayerChat", function(ply, text) -- teamChat, isDead
 if not ply:IsValid() then
     -- Exit if player is not valid (it is a console or unconnected player).
-    return
-end
+        return
+    end
 login_sys.temp = text:Trim():gsub("%s+", " ") -- Trim submitted text and replace all spaces with a single space.
 if login_sys.temp:Left(1) ~= login_sys.chat_prefix then
     -- Exit when first character does not match chat prefix.
-    return
-end
+        return
+    end
 login_sys.tempe = string.Explode(" ", login_sys.temp) -- Explode by space.
 if login_sys.tempe[1] == login_sys.chat_prefix then
     -- In case if the first exploded element matches the chat prefix.
@@ -57,11 +84,11 @@ if login_sys.tempe[1] == login_sys.chat_prefix then
     end
     -- Uppercase and concate second element with the chat prefix (first element).
     login_sys.tempe[1] = login_sys.tempe[1] .. login_sys.tempe[2]:upper()
-    -- And remove the second element from the table
+    -- And remove the second element from the table.
     table.remove(login_sys.tempe, 2)
 else
     -- Assuming the first element starts with chat prefix.
-    if #login_sys.tempe < 1 then
+    if #login_sys.tempe < 1 or login_sys.tempe[1]:Left(1) ~= login_sys.chat_prefix then
         return
     end
     -- Uppercase the first element.
@@ -88,25 +115,36 @@ login_sys.switch(login_sys.tempe[1],
         print(name)
     end, true))
 end)
+hook.Add("HUDPaint", login_sys.hook_prefix .. "OnScreenResolutionChange", function()
+    local local_player = LocalPlayer()
+    if not IsValid(local_player) then
+        return
+    end
+    local should_draw = hook.Call("HUDShouldDraw", GAMEMODE, login_sys.hook_prefix .. "OnScreenResolutionChange")
+    if not should_draw then
+        return
+    end
+    if not local_player.screen_resolution then
+        local_player.screen_resolution =
+        {
+            width = ScrW(),
+            height = ScrH()
+        }
+    end
+    local new_width, new_height = ScrW(), ScrH()
+    if new_width ~= local_player.screen_resolution.width or new_height ~= local_player.screen_resolution.height then
+        hook.Call("OnScreenResolutionChanged", GAMEMODE)
+        local_player.screen_resolution.width = new_width
+        local_player.screen_resolution.height = new_height
+    end
+end)
+hook.Add("OnScreenResolutionChanged", login_sys.hook_prefix .. "OnScreenResolutionChanged", function()
+    if not login_sys.convars.vgui or not login_sys.convars.vgui:GetBool() or not login_sys.vgui.dframe then
+        return
+    end
+    login_sys.vgui.dframe:Close()
+    RunConsoleCommand(login_sys.convars.vgui:GetName(), "1")
+end)
 DebugInfo(1, "Login System: Enabled.")
-end, true, true, true) -- , notify, persist, forceExecution
-login_sys.convars.vgui = cvarsx.CreateTwoStateClientConVar(login_sys.convar_prefix .. "vgui", 0, false, false, nil, function(convar_name) -- value_old, value_new
-if not IsValid(GetHUDPanel()) then
-    return
-end
-local dframe = vgui.Create("DFrame", GetHUDPanel())
-dframe.btnMaxim:SetVisible(false) -- Hide maximize button.
-dframe.btnMinim:SetVisible(false) -- Hide minimize button.
-dframe:SetBackgroundBlur(true)
---dframe:SetDraggable(false)
-dframe:SetScreenLock(true)
-dframe:SetSize(616, 456)
-dframe:SetTitle("Login System VGUI")
-dframe:SetZPos(32767)
-dframe:Center()
-dframe:MakePopup()
-dframe.OnClose = function()
-    RunConsoleCommand(convar_name, 0) -- Reset vgui convar.
-end
 end, true, true, true)
 collectgarbage()
--- lua/includes/modules/cvarsx.lua
+++ lua/includes/modules/cvarsx.lua
@@ -13,8 +13,8 @@ module("cvarsx", package.seeall)
 -- funcOff(function): The function that is run when convar value changes to 0.
 -- funcOn(function): The function that is run when convar value chanes to 1.
 -- notify(boolean): Prints client convar change.
--- persist(boolean): Console variable will be reset to the default value if set to false; otherwise it will keep the current value after re-opening/executing script.
--- forceExecution(boolean): This is new parameter which will force execution of the function for the current state.
+-- persist(boolean): Console variable will be reset to the default value if set to false; otherwise it will keep the current value after re-opening/executing a Lua script.
+-- forceExecution(boolean): This is new parameter which will force execution of the function for the default state.
 CreateTwoStateClientConVar = function(name, default, shouldsave, userdata, funcOff, funcOn, notify, persist, forceExecution)
     local func_name = debug.getinfo(1, "n").name
     assert(type(name) == "string", Format("bad argument %d# to '%s' (string expected, got %s)", 1, func_name, type(name)))
@@ -41,31 +41,42 @@ CreateTwoStateClientConVar = function(name, default, shouldsave, userdata, funcO
     end
     -- Create a new callback for the convar.
     cvars.AddChangeCallback(cvar:GetName(), function(convar_name, value_old, value_new)
-        value_old = math.Clamp(math.floor(tonumber(value_old) or default), 0, 1)
-        print(Format("value_old=%s", value_old))
-        value_new = math.Clamp(math.floor(tonumber(value_new) or default), 0, 1)
-        print(Format("value_new=%s", value_new))
-        if value_old == value_new then
-            RunConsoleCommand(convar_name, value_new) -- set it again, and exit the function
+        value_old = math.floor(tonumber(value_old))
+        value_new = math.floor(tonumber(value_new) or default)
+        if (value_old < 0 and value_new < 0) or (value_old < 0 and value_new == 0) or (value_old == 0 and value_new < 0) or (value_old == 0 and value_new == 0) then
+            print("already off")
+            RunConsoleCommand(convar_name, "0")
             return
         end
-        if value_new == 0 then
-            --print("value_new == 0")
-            if funcOff and type(funcOff) == "function" then
-                funcOff(convar_name, value_old, value_new)
-            end
-            if notify and type(notify) == "boolean" then
-                print("Client cvar \"" .. convar_name .. "\" has changed to 0.")
-            end
-        else --if value_new == 1 then
-        --print("value_new == 1")
+        if (value_old > 1 and value_new > 1) or (value_old > 1 and value_new == 1) or (value_old == 1 and value_new > 1) or (value_old == 1 and value_new == 1) then
+            print("already on")
+            RunConsoleCommand(convar_name, "1")
+            return
+        end
+        if (value_old < 0 and value_new > 1) or (value_old < 0 and value_new == 1) or (value_old == 0 and value_new > 1) or (value_old == 0 and value_new == 1) then
+            print("turn it on")
             if funcOn and type(funcOn) == "function" then
                 funcOn(convar_name, value_old, value_new)
             end
             if notify and type(notify) == "boolean" then
                 print("Client cvar \"" .. convar_name .. "\" has changed to 1.")
             end
+            RunConsoleCommand(convar_name, "1")
+            return
         end
+        if (value_old > 1 and value_new < 0) or (value_old == 1 and value_new < 0) or (value_old > 1 and value_new == 0) or (value_old == 1 and value_new == 0) then
+            print("turn it off")
+            if funcOff and type(funcOff) == "function" then
+                funcOff(convar_name, value_old, value_new)
+            end
+            if notify and type(notify) == "boolean" then
+                print("Client cvar \"" .. convar_name .. "\" has changed to 0.")
+            end
+            RunConsoleCommand(convar_name, "0")
+            return
+        end
+        print(value_old, value_new)
+        RunConsoleCommand(convar_name, tostring(math.Clamp(value_new, 0, 1)))
     end, cvar:GetName()) -- ConVarName, FunctionCallback, Identifier
     -- Persist the current state, else set default state. And execute appropriate function depending on the state.
     if persist and type(persist) == "boolean" then
@@ -87,7 +98,7 @@ CreateTwoStateClientConVar = function(name, default, shouldsave, userdata, funcO
                 end
             end
         end
-        RunConsoleCommand(cvar:GetName(), value)
+        RunConsoleCommand(cvar:GetName(), tostring(value))
     else
         if forceExecution then
             if default == 1 then
@@ -106,7 +117,7 @@ CreateTwoStateClientConVar = function(name, default, shouldsave, userdata, funcO
                 end
             end
         end
-        RunConsoleCommand(cvar:GetName(), default)
+        RunConsoleCommand(cvar:GetName(), tostring(default))
     end
     return cvar
 end
